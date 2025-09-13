f2cdf9d5a18bf20eaae34b56400fbb59df1a71bc
tashaljordan-dev <tashaljordan@gmail.com>
2025-09-13 18:33:42 +0000

    Initial JS app

---
+++ app.js
@@ -0,0 +1,20 @@
+let result = "5" - 2;
+console.log("The result is: " + result);
+
+let isValid = Boolean("false");
+if (isValid) {
+    console.log("This is valid!");
+}
+
+let age = 25;
+let totalAge = age + 5;
+console.log("Total Age: " + totalAge);
+
+let num = 5678
+let str = String(num);
+
+console.log(str);
+console.log(typeof str);
+
+let answer = "The answer is: " + 42;
+console.log(answer);
